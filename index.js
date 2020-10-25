7a2ecd728611c6abeb64aaad1c4f839610c7f2f6
Bárbara <babyr.oliveira@gmail.com>
2020-10-25 17:02:45 -0300

    Exercício em desenvolvimento.

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const cidadeseestados = [{
+const cidadeseestados = {
     PR : ['Curitiba', 'Cascavel', 'Maringá'],
     SC : ['Florianópolis', 'Blumenau', 'Lages'],
     RS : ['Porto Alegre', 'Gramado', 'Caxias do Sul']
-}]
+}
 
 let estadoselecionado;
 
-const $select = document.getElementById ('estado');
+const $selecEstado = document.querySelector('#estado');
+const $selectCidade = document.querySelector('#cidade');
 
-$select.onchange = (e) => {
-	console.log(e);
-}
+$selectEstado.addEventListener ('change', (e) => {
+    estadoselecionado = e.target.value;
+    const cidadesDoEstado = cidadesseestados[estadoselecionado];
+
+    if($selectCidade.hasAttribute ('disabled')) $selectCidade.removeAttribute('disable');
+
+    $selectCidade.innerHTML = `<option value='inicio'>Selecione um estado</option`;
+
+    cidadesDoEstado.forEach(cidade => {
+        $selectCidade.innerHTML += `<option value=${cidade}>${cidade}</option>`;
+    });
+}, false);
 
 
 
